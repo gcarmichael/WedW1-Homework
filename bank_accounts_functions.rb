1cc3f999595f1a3ddf9d791fdd71065a3a586855
Gary Carmichael <g.carmichael90@gmail.com>
2015-11-18 20:46:50 +0000

    Completed.

--- bank_accounts_functions.rb
+++ bank_accounts_functions.rb
@@ -63,7 +63,50 @@ def total_cash_in_bank
   total_amount
 end
 
+def last_bank_account_holder()
+  ACCOUNTS.last[:holder_name]
+end
 
+def aveage_cash_in_bank()
+  average_amount = 0
+  for account in ACCOUNTS
+    average_amount = average_amount + account[:amount]
+  end
+  average_amount / ACCOUNTS.length
+end
 
+def total_business_cash_in_bank()
+  total_business_amount = 0
+  for account in ACCOUNTS
+    if account[:type] == "business"
+    total_business_amount = total_business_amount + account[:amount]
+   end
+  end
+  total_business_amount
+end
 
+def holder_of_largest_account()
+#   for account in ACCOUNTS
+#     if account[:amount].max
+#       largest = account[:amount]
+#     end
+#   largest
+# end
+  # ACCOUNTS.max[:amount]
+  name = ACCOUNTS.select{|x| x[:holder_name]}
+                 .max_by{|x| x[:amount]}
+  return name[:holder_name]
+end
+
+def holder_of_largest_personal_account()
+  name = ACCOUNTS.select{|x| x[:type] == "personal"}
+                 .max_by{|x| x[:amount]}
+  return name[:holder_name]
+end
+
+def holder_of_largest_business_account()
+  name = ACCOUNTS.select{|x| x[:type] == "business"}
+                 .max_by{|x| x[:amount]}
+  return name[:holder_name]
+end
 
--- bank_accounts_runner.rb
+++ bank_accounts_runner.rb
@@ -21,14 +21,33 @@ puts "total in bank #{total_cash}"
 puts "\n"
 
 ###last account owner
+last_bank_owner = last_bank_account_holder()
+puts "last bank account owner is #{last_bank_owner}"
+puts "\n"
 
 ###average bank account value
+average_cash = aveage_cash_in_bank()
+puts "average cash #{average_cash}"
+puts "\n"
 
 ###total cash in business accounts
+total_business = total_business_cash_in_bank()
+puts "total business accounts cash in bank #{total_business}"
+puts "\n"
 
 ###holder of largest bank account
+largest_account = holder_of_largest_account()
+puts "largest account holder is #{largest_account}"
+puts "\n"
 
 ###holder of largest personal account
+largest_personal_account = holder_of_largest_personal_account()
+puts "largest personal account holder is #{largest_personal_account}"
+puts "\n"
 
 ###any other functionality you want to add.
 
+##holder of largest business account
+largest_business_account = holder_of_largest_business_account()
+puts "largest business account holder is #{largest_business_account}"
+puts "\n"
